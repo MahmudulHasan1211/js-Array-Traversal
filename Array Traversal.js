f07eb6e3169b78d3dc786fb1abce156b1e33593d
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-09 22:27:13 +0600

    length print addded

--- Array Traversal.js
+++ Array Traversal.js
@@ -6,7 +6,7 @@ for (const friend of friends){
 //   console.log(friend);
 }
 
-for (let i = 0; i<=11; i++){
+for (let i = 0; i <= friends.length; i++){
     console.log(i)
-  
+  console.log(friends[i])
 }
