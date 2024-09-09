86f3248756b3d4f63d2efe387ad0b62c3e184993
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-09 22:32:35 +0600

    new print way added

--- Array Traversal.js
+++ Array Traversal.js
@@ -1,4 +1,4 @@
-// fpr of 
+// for of 
 
 const friends = ['Elon', 'Bill', 'hasan', 'mark'];
 
@@ -8,5 +8,11 @@ for (const friend of friends){
 
 for (let i = 0; i <= friends.length; i++){
     console.log(i)
-  console.log(friends[i])
+//   console.log(friends[i])
+}
+
+let numbers = [5, 7, 8, 44, 43, 33, 555, 55];
+
+for (let i = 3; i <= numbers.length; i++){
+    console.log(numbers[i])
 }
