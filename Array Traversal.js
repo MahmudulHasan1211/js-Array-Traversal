1933a4477a914620c2aef693e9b8617cb8fdcc89
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-09 22:40:06 +0600

    for and while loop added

--- Array Traversal.js
+++ Array Traversal.js
@@ -3,16 +3,25 @@
 const friends = ['Elon', 'Bill', 'hasan', 'mark'];
 
 for (const friend of friends){
-//   console.log(friend);
+  console.log(friend);
 }
 
 for (let i = 0; i <= friends.length; i++){
     console.log(i)
-//   console.log(friends[i])
+  console.log(friends[i])
 }
 
 let numbers = [5, 7, 8, 44, 43, 33, 555, 55];
 
+// for loop 
 for (let i = 3; i <= numbers.length; i++){
     console.log(numbers[i])
+}
+
+let i = 0;
+
+// while loop 
+while(i < numbers.length){
+    console.log(numbers[i]);
+    i++
 }
