f0341df6c3c3b66564e18ed188a4cd7997407c3b
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-09 22:16:28 +0600

    first commit

---
+++ Array Traversal.js
@@ -0,0 +1,12 @@
+// fpr of 
+
+const friends = ['Elon', 'Bill', 'hasan', 'mark'];
+
+for (const friend of friends){
+//   console.log(friend);
+}
+
+for (let i = 0; i<=11; i++){
+    console.log(i)
+  
+}
